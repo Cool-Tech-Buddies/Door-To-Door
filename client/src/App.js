a09341a6d7b0a66dfef8916c9e334141bd6628c6
MathiarasiNagaraj <mathiarasinagaraj05@gmail.com>
2022-01-30 12:25:36 +0530

    Landing page works

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import LandingPage from './pages/LandingPage'
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <LandingPage></LandingPage>
+   
   );
 }
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    FontFamily:{
+'popin':'Poppins, sans-serif',
+'body':'Josefin Sans, sans-serif',
+'header':'lora',
+'text':'Slabo 27px'
+    }
+  },
+  plugins: [],
+}
